40de5c45fc586791943285e5232b53cdcdbfdfa9
Larissa <larissa.vianna@embeddo.io>
2025-04-15 14:41:57 -0300

    first commit

---
+++ client/components/loginNavbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {logo} from '../assets/UbuyLogo2.png'
+
+const loginNavbar = () => {
+  return (
+    <div className='container-navbar'>
+      {logo}
+    </div>
+  )
+}
+
+export default loginNavbar
--- client/pages/Login.jsx
+++ client/pages/Login.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import loginNavbar from '../components/loginNavbar.jsx'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+      <loginNavbar />
+    </div>
   )
 }
 
